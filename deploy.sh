21343dacffd348e746dc130fb540f7a0779e4a49
JiangWeixian <saber_wei@163.com>
2019-02-26 10:18:16 +0800

    rm readme.md

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
+# 终止一个错误
 set -e
 
-# 生成静态文件
+# 构建
 npm run build
 
-# 进入生成的文件夹
+# 进入生成的构建文件夹
 cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
+# 如果你是要部署到自定义域名
 # echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
+# 如果你想要部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# 如果你想要部署到 https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/JiangWeixian/articles.git master:gh-pages
 
 cd -
